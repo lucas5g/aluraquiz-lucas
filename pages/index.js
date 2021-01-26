723858bcf5063eddef702e4e9d7a407a355dbe18
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-01-26 00:02:38 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
+import db from '../db.json'
 import styled from 'styled-components'
+import  Widget  from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GithubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+
+export const QuizContainer = styled.div`
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width: 500px){
+        margin: auto;
+        padding: 15px;
+    }
+
 `
 
-export default function Home() {
-  return <Title>My page</Title>
+function Home() {
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <Widget>
+                    <Widget.Header>
+                        <h1>teste</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+
+
+                        <p>lorem</p>
+                    </Widget.Content>
+                </Widget>
+                <Footer />
+            </QuizContainer>
+            <GitHubCorner projectUrl='https:github.com/lucas5g'/>
+
+        </QuizBackground>
+    )
 }
+
+export default Home
