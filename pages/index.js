f99f34348545bac6d0871b33d4367cfc3d22f447
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-01-26 23:55:13 -0300

    aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import db from '../db.json'
-import styled from 'styled-components'
-import  Widget  from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GithubCorner'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router'
 
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GithubCorner';
+import { route } from 'next/dist/next-server/server/router';
 
 export const QuizContainer = styled.div`
     width: 100%;
@@ -16,28 +21,47 @@ export const QuizContainer = styled.div`
         padding: 15px;
     }
 
-`
+`;
 
 function Home() {
+
+    const router = useRouter()
+    const [name, setName] = useState('')
     return (
         <QuizBackground backgroundImage={db.bg}>
+            <Head>
+                <title>
+                    AluraQuiz - Modelo Base
+        </title>
+            </Head>
             <QuizContainer>
                 <Widget>
                     <Widget.Header>
-                        <h1>teste</h1>
+                        <h1>The Legend of zelda</h1>
                     </Widget.Header>
                     <Widget.Content>
+                        <form onSubmit={ (event ) => {
+                            event.preventDefault()
+                            router.push(`/quiz?name=${name}`)
+                            console.log('teste')
+                        }}>
+                            <input placeholder="Dia ai seu nome" onChange={(event) => {
+                                const {value} = event.target
+                                setName(value)
+                            }} />
+                            <button type="submit" disabled={name.length === 0}>
+                                Jogar {name}
+                            </button>
 
-
-                        <p>lorem</p>
+                        </form>
                     </Widget.Content>
                 </Widget>
                 <Footer />
             </QuizContainer>
-            <GitHubCorner projectUrl='https:github.com/lucas5g'/>
+            <GitHubCorner projectUrl="https:github.com/lucas5g" />
 
         </QuizBackground>
-    )
+    );
 }
 
-export default Home
+export default Home;
