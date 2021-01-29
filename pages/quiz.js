727734bbde6f7d9b17437f2ed5dc4b48a0b494fa
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-01-29 00:16:16 -0300

    aula 3 52:05

--- pages/quiz.js
+++ pages/quiz.js
@@ -21,19 +21,14 @@ function LoadingWidget() {
     );
 }
 
-function QuestionWidget({
-    question,
-    questionIndex,
-    totalQuestions,
-    onSubmit,
-}) {
-    const questionId = `question__${questionIndex}`;
+function QuestionWidget({ question, questionIndex, totalQuestions }) {
+    // const questionId = `question__${questionIndex}`;
+    // console.log(question)
     return (
         <Widget>
             <Widget.Header>
-                {/* <BackLinkArrow href="/" /> */}
                 <h3>
-                    {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+                    Pergunta {questionIndex + 1} de {totalQuestions}
                 </h3>
             </Widget.Header>
 
@@ -46,6 +41,7 @@ function QuestionWidget({
                 }}
                 src={question.image}
             />
+
             <Widget.Content>
                 <h2>
                     {question.title}
@@ -53,38 +49,20 @@ function QuestionWidget({
                 <p>
                     {question.description}
                 </p>
-
-                <form
-                    onSubmit={(infosDoEvento) => {
-                        infosDoEvento.preventDefault();
-                        onSubmit();
-                    }}
-                >
-                    {question.alternatives.map((alternative, alternativeIndex) => {
-                        const alternativeId = `alternative__${alternativeIndex}`;
-                        return (
-                            <Widget.Topic
-                                as="label"
-                                htmlFor={alternativeId}
-                            >
-                                <input
-                                    // style={{ display: 'none' }}
-                                    id={alternativeId}
-                                    name={questionId}
-                                    type="radio"
-                                />
-                                {alternative}
-                            </Widget.Topic>
-                        );
-                    })}
-
-                    {/* <pre>
-            {JSON.stringify(question, null, 4)}
-          </pre> */}
-                    <Button type="submit">
-                        Confirmar
-          </Button>
-                </form>
+                {/* {console.log(question.alternatives[0])} */}
+                {question.alternatives.map((alternative) => 
+                    <p>
+                        {alternative}
+                    </p>
+                )}
+                {/* <pre>
+                    {JSON.stringify(question, null, 4)}
+                </pre> */}
+                <Button 
+                    type="submit"
+                    >
+                    Confirmar
+                </Button>
             </Widget.Content>
         </Widget>
     );
@@ -96,89 +74,24 @@ const screenStates = {
     RESULT: 'RESULT',
 };
 export default function QuizPage() {
-    const [screenState, setScreenState] = React.useState(screenStates.LOADING);
-    const totalQuestions = db.questions.length;
-    const [currentQuestion, setCurrentQuestion] = React.useState(0);
-    const questionIndex = currentQuestion;
-    const question = db.questions[questionIndex];
-
-    // [React chama de: Efeitos || Effects]
-    // React.useEffect
-    // atualizado === willUpdate
-    // morre === willUnmount
-    // React.useEffect(() => {
-    //     // fetch() ...
-    //     setTimeout(() => {
-    //         // setScreenState('quiz')
-    //         setScreenState(screenStates.QUIZ);
-    //         console.log(screenStates.QUIZ)
-    //     }, 1 * 1000);
-    //     // nasce === didMount
-    // }, []);
-
-    // function handleSubmitQuiz() {
-    //     const nextQuestion = questionIndex + 1;
-    //     if (nextQuestion < totalQuestions) {
-    //         setCurrentQuestion(nextQuestion);
-    //     } else {
-    //         setScreenState(screenStates.RESULT);
-    //     }
-    // }
-
+    // console.log('Perguntas criadas: ', db.questions)
+    const totalQuestions = db.questions.length
+    const questionIndex = 0
+    const question = db.questions[questionIndex]
     return (
-        <QuizBackground backgroundImage={db.bg}>
+        <QuizBackground>
             <QuizContainer>
                 <QuizLogo />
-                {/* {screenState === screenStates.QUIZ && (
-                    <QuestionWidget
-                        question={question}
-                        questionIndex={questionIndex}
-                        totalQuestions={totalQuestions}
-                        onSubmit={handleSubmitQuiz}
-                    />
-                )}
+                <QuestionWidget
+                    question={question}
+                    questionIndex={questionIndex}
+                    totalQuestions={totalQuestions}
 
-                {screenState === screenStates.LOADING && <LoadingWidget />}
+                />
 
-                {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>} */}
-                <Widget>
-                    <Widget.Header>
-
-                        <h3>
-
-                            Pergunta
-                            1
-                            de
-                    {`${db.questions.length}`}
-                        </h3>
-                    </Widget.Header>
-
-                    <img
-                        alt="Descrição"
-                        style={{
-                            width: '100%',
-                            height: '150px',
-                            objectFit: 'cover',
-                        }}
-                        src="https://placehold.it/400X400"
-
-
-                    />
-                    <Widget.Content>
-                        <h2>
-                            Título
-                        </h2>
-                        <p>
-                            Descrição
-                        </p>
-                        <Button>
-                            Confirmar
-                        </Button>
-                    </Widget.Content>
-                </Widget>
 
                 <LoadingWidget />
             </QuizContainer>
-        </QuizBackground>
+        </QuizBackground >
     );
 }
